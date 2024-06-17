8b1321c5fefd65fa7883f0e9583f48c091bf2759
Andriy <qermonqermon@gmail.com>
2024-06-17 20:31:23 +0300

    first

--- women/views.py
+++ women/views.py
@@ -45,7 +45,7 @@ logger = logging.getLogger(__name__)
 
 class Search(ListView):
     template_name = 'women/menu.html'
-    context_object_name = 'dishes'git init
+    context_object_name = 'dishes'
 
     def get_queryset(self):
         query = self.request.GET.get('q')
